381a97e0adc20abf895f024d362720ce745499a9
Vignesh2141 <73704616+Vignesh2141@users.noreply.github.com>
2021-09-16 12:56:02 +0530

    new commit

---
+++ GitHubTestProject/src/com/neosoft/MainClass.java
@@ -0,0 +1,16 @@
+package com.neosoft;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+	 int s=0;
+	 if(s==0) {
+		 System.out.println("you are a Software Engineer");
+	 }
+	 else {
+		 System.out.println("You are not a software Engineer");
+	 }
+
+	}
+
+}
